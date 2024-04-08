bb628ee48ff2ecb97ed11cedb62d8ed44c32f406
Ashok <ashokjogi630@gmail.com>
2024-04-08 15:20:02 +0530

    second commit

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const imagesData = [
       {id :104, imgurl:"https://res.cloudinary.com/dhtdkkae1/image/upload/v1712565329/Lotus_temple_xharyq.jpg", description:"The Lotus Temple, located in New Delhi, India, is a Bahá'í House of Worship and one of the most distinctive architectural landmarks in the world. It is known for its unique lotus flower-inspired design and serves as a place of worship for people of all faiths."},
       {id :105, imgurl:"https://res.cloudinary.com/dhtdkkae1/image/upload/v1712565191/Himalaya_p4ksc0.jpg", description:"The Himalayas are a vast mountain range in Asia, stretching across five countries: India, Nepal, Bhutan, China (Tibet), and Pakistan. They are home to some of the world's highest peaks, including Mount Everest, the highest point on Earth."},
       {id :106, imgurl:"https://res.cloudinary.com/dhtdkkae1/image/upload/v1712565871/rajasthanu_t0nc5t.jpg", description:"Rajasthan is a state in northwestern India known for its diverse landscapes, which include the Thar Desert, also known as the Great Indian Desert. Here are some key points about the desert region within Rajasthan"},
+      {id : 107, imgurl:"https://res.cloudinary.com/dhtdkkae1/image/upload/v1712569659/redfort3_xhqvdd.jpg", description:"The Red Fort, also known as Lal Qila in Hindi, is a historic fort located in the city of Delhi, India. It is one of the most iconic landmarks in the country and holds significant historical and cultural importance. Here are some key points about the Red Fort"}
     ];
   
 
